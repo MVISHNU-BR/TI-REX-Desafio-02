d3e56839dff3007694b8db245f0b3858402f046e
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-01-06 19:36:25 -0300

    Feat component header

---
+++ project-manager/src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router";
+import logo from "../assets/logo.svg";
+import Button from "./Button";
+export default function Header() {
+  return (
+    <>
+      <header className="bg-azul-escuro w-full min-h-20 h-full flex items-center justify-between gap-2">
+        <Link
+          to="/"
+          className="flex gap-5 items-center pl-5 
+        "
+        >
+          <img src={logo} width={50} height={50} alt="logo project manager" />
+          <h1 className="text-white text-lg sm:text-xl lg:text-[28px] text-nowrap">
+            Project Manager
+          </h1>
+        </Link>
+        <div className="flex items-center justify-center pr-5">
+          <Button />
+        </div>
+      </header>
+    </>
+  );
+}
--- project-manager/src/pages/Home.tsx
+++ project-manager/src/pages/Home.tsx
@@ -1,6 +1,10 @@
+import Header from "../components/Header";
+
 function Home() {
   return (
     <>
+      <Header />
+      <h1 className="">Home</h1>
     </>
   );
 }
