d7a1895b8a925d7e079e8a57d961400cbe2946d8
Gerson Junior <90159113+GersonJrr@users.noreply.github.com>
2025-01-08 11:49:58 -0300

    feat: login button
    login button

---
+++ project-manager/src/components/LoginButton.tsx
@@ -0,0 +1,17 @@
+export default function LoginButton() {
+ 
+  
+ 
+    return (
+        <button className="
+        w-[105px] h-[36px] rounded-[30px] border border-gray-300 
+        flex items-center justify-center gap-[8px] text-branco 
+        text-sm md:w-[162px] md:h-[48px] md:gap-[12px] md:text-base"
+        >
+        Login
+      </button>
+   
+    );
+}
+  
+
