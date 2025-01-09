83719fbd6ddce5b3b08a9bb3020284f8cae28dd6
Gerson Junior <90159113+GersonJrr@users.noreply.github.com>
2025-01-09 17:00:02 -0300

    update: button login and Button Get started
    update style button login and Button Get started

--- project-manager/src/components/Button.tsx
+++ project-manager/src/components/Button.tsx
@@ -1,9 +1,15 @@
 export default function Button() {
   return (
     <>
-      <button className="bg-white rounded-lg px-4 py-2 flex justify-">
-        Teste de botão
+      <button className="
+        w-[118px] h-[36px] rounded-[30px] border border-gray-300 
+        flex items-center justify-center gap-[8px] text-branco 
+        text-[15px] leading-[24px] font-bold 
+        md:w-[162px] md:h-[48px] md:gap-[12px] md:text-base 
+        hover:border-azul-hover transition duration-300">
+        Get Started
       </button>
+
     </>
   );
 }
