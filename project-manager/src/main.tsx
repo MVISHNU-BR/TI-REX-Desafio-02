03bb681f86f59b36b9ed314c1152704203ecd5d0
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-01-10 18:02:39 -0300

    Feat: routing on main and add notfaund page

--- project-manager/src/main.tsx
+++ project-manager/src/main.tsx
@@ -6,7 +6,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { ClerkProvider } from "@clerk/react-router";
-import ProtectedDashboard from "./Routes/AuthDashboard";
+import ProtectedDashboard from "./Routes/AuthRoutes";
+import NotFound from "./pages/NotFound";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -19,10 +20,11 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/dashboard" element={<ProtectedDashboard />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<ProtectedDashboard />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </ClerkProvider>
     </StrictMode>
--- project-manager/src/pages/NotFound.tsx
+++ project-manager/src/pages/NotFound.tsx
@@ -2,43 +2,45 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Link } from "react-router";
 
-
 export default function NotFound() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow flex items-center justify-center">
-            <div className="w-[956px] h-[439px] flex items-center justify-center overflow-hidden">
-                <div className="flex items-center gap-8 w-full h-full">
-                    <img 
-                        src="src/assets/404.png" 
-                        alt="Post-it" 
-                        className="w-[227px] h-[220px] md:w-[455px] md:h-[439px] max-w-full max-h-full object-contain"
-                    />
-                    <div className="w-full h-full flex flex-col justify-center">
-                        <h1 className="font-roboto mb-4" style={{ fontSize: "88px" }}>
-                        <h1 className="font-roboto mb-4" style={{fontSize: "88px"}}>
-                            404
-                        </h1>
-                        <p className="text-lg text-black-500 mb-4 word-break" style={{ fontSize: "24px" }}>
-                        <p className="text-lg text-black-500 mb-4" style={{fontSize: "24px"}}>
-                            Oops! Nothing to see here, just a lost kanban task.
-                        </p>
-                        <Link to="/" className="text-blue-500 text-right hover:underline font-semibold">
-                            Go back to home 
-                        </Link>
-                        <div className="w-full text-right">
-                          <Link to="/" className="text-blue-500 underline font-semibold">
-                          Go back to home 
-                          </Link>
-                        </div>
-                    </div>
-                </div>
+        <div className="w-[956px] h-[439px] flex items-center justify-center overflow-hidden">
+          <div className="flex items-center gap-8 w-full h-full">
+            <img
+              src="src/assets/404.png"
+              alt="Post-it"
+              className="w-[227px] h-[220px] md:w-[455px] md:h-[439px] max-w-full max-h-full object-contain"
+            />
+            <div className="w-full h-full flex flex-col justify-center">
+              <h1 className="font-roboto mb-4" style={{ fontSize: "88px" }}>
+                404
+              </h1>
+              <p
+                className="text-lg text-black-500 mb-4"
+                style={{ fontSize: "24px" }}
+              >
+                Oops! Nothing to see here, just a lost kanban task.
+              </p>
+              <Link
+                to="/"
+                className="text-blue-500 text-right hover:underline font-semibold"
+              >
+                Go back to home
+              </Link>
+              <div className="w-full text-right">
+                <Link to="/" className="text-blue-500 underline font-semibold">
+                  Go back to home
+                </Link>
+              </div>
             </div>
-    </main>
+          </div>
+        </div>
+      </main>
 
       <Footer />
     </div>
   );
 }
-
