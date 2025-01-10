18b7c840a53b7abaa812cd7adac4ce8004c05e72
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-01-10 15:20:41 -0300

    Fix: change logout button to correct place

--- project-manager/src/pages/Dashboard.tsx
+++ project-manager/src/pages/Dashboard.tsx
@@ -1,16 +1,15 @@
-import { useUser, useClerk } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
 import Header from "../components/Header";
+import HeaderMenu from "../components/HeaderMenu";
 
 export default function Dashboard() {
   const { user } = useUser();
-  const { signOut } = useClerk();
   return (
     <>
-      <Header />
+      <Header>
+        <HeaderMenu></HeaderMenu>
+      </Header>
       <h1 className="text-2xl">Bem-vindo(a), {user?.fullName || "Usuário"}</h1>
-      <button onClick={() => signOut()} className="btn btn-primary mt-4">
-        Logout
-      </button>
     </>
   );
 }
