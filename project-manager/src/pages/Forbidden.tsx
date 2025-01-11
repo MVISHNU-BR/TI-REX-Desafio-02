cd7cc629fd227ef67a91dec79770c25c917cf49b
Maiara Lira <maiaraslira@gmail.com>
2025-01-11 13:07:29 -0300

    Fix: review and refinement of font styles and sizes.

--- project-manager/src/pages/Forbidden.tsx
+++ project-manager/src/pages/Forbidden.tsx
@@ -1,14 +1,8 @@
-import { useNavigate } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Link } from "react-router";
 
 export default function Forbidden() {
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    navigate("/Login"); 
-  };
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -18,16 +12,17 @@ export default function Forbidden() {
             <img
               src="src/assets/403.png"
               alt="Padlock"
-              className="w-[227px] h-[220px] md:w-[367px] md:h-[367px] max-w-full max-h-full object-contain"/>
+              className="w-[227px] h-[220px] md:w-[367px] md:h-[367px] max-w-full max-h-full object-contain"
+            />
             <div className="w-full h-full flex flex-col justify-center">
-              <h1 className="font-roboto mb-2 " style={{ fontSize: "96px" }}>
+              <h1 className="font-roboto text-[96px] mb-2" >
                 403
               </h1>
-              <p className="font-roboto text-lg text-black-500 font-normal mb-2" style={{ fontSize: "24px" }}>
+              <p className="font-roboto text-[24px] text-black font-normal mb-2">
                 Nothing to see here... yet! To see this content, you need to {" "}
-                <a href="#" onClick={handleLoginClick} className="text-blue-700 font-medium underline">
+                <Link to="/Login" className="text-blue-700 font-medium underline">
                   log in.
-                </a>
+                </Link>
               </p>
             </div>
           </div>
--- project-manager/src/pages/NotFound.tsx
+++ project-manager/src/pages/NotFound.tsx
@@ -16,27 +16,21 @@ export default function NotFound() {
                         className="w-[227px] h-[220px] md:w-[455px] md:h-[439px] max-w-full max-h-full object-contain"
                     />
                     <div className="w-full h-full flex flex-col justify-center">
-                        <h1 className="font-roboto mb-4" style={{ fontSize: "88px" }}>
-                        <h1 className="font-roboto mb-4" style={{fontSize: "88px"}}>
+                        <h1 className="font-roboto mb-4 text-9xl" >
                             404
                         </h1>
-                        <p className="text-lg text-black-500 mb-4 word-break" style={{ fontSize: "24px" }}>
-                        <p className="text-lg text-black-500 mb-4" style={{fontSize: "24px"}}>
+                        <p className="w-full text-black text-2xl word-break mb-4">
                             Oops! Nothing to see here, just a lost kanban task.
                         </p>
-                        <Link to="/" className="text-blue-500 text-right hover:underline font-semibold">
-                            Go back to home 
-                        </Link>
-                        <div className="w-full text-right">
-                          <Link to="/" className="text-blue-500 underline font-semibold">
+                        <div className="w-full max-w-[395px] lg:w-[395px] xl:text-[20px] text-right">
+                          <Link to="/" className="text-blue-700 underline font-bold">
                           Go back to home 
                           </Link>
-                        </div>
-                    </div>
+                      </div>
                 </div>
+              </div>
             </div>
     </main>
-
       <Footer />
     </div>
   );
