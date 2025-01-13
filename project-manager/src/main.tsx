476190dff7df6eebc62b922d49bbe04f3e060780
Mahavishnu Jn <mahajneesh@hotmail.com>
2025-01-13 11:51:38 -0300

    Feat: add pages and fix private routes.

--- project-manager/src/main.tsx
+++ project-manager/src/main.tsx
@@ -6,8 +6,11 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { ClerkProvider } from "@clerk/react-router";
-import ProtectedDashboard from "./Routes/AuthRoutes";
+import ProtectedRoutes from "./Routes/AuthRoutes";
 import NotFound from "./pages/NotFound";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -20,10 +23,18 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
         <Routes>
+          {/* rotas publicas */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<ProtectedDashboard />} />
+
+          {/* rotas protegidas */}
+          <Route path="/" element={<ProtectedRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          {/* rota padrão para páginas não encontradas */}
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </ClerkProvider>
