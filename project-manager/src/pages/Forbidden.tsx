42713f6bcf3a927650c1923c8c0eecfb88416de7
Gerson Junior <90159113+GersonJrr@users.noreply.github.com>
2025-01-14 21:15:02 -0300

    fix: responsividade pag Forbidden
    responsividade pag Forbidden

--- project-manager/src/pages/Forbidden.tsx
+++ project-manager/src/pages/Forbidden.tsx
@@ -4,31 +4,44 @@ import { Link } from "react-router";
 
 export default function Forbidden() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow flex items-center justify-center">
-        <div className="w-[845px] h-[367px] flex items-center justify-center overflow-hidden">
-          <div className="flex items-center gap-6 w-full h-full">
-            <img
-              src="src/assets/403.png"
-              alt="Padlock"
-              className="w-[227px] h-[220px] md:w-[367px] md:h-[367px] max-w-full max-h-full object-contain"
-            />
-            <div className="w-full h-full flex flex-col justify-center">
-              <h1 className="font-roboto text-[96px] mb-2" >
-                403
-              </h1>
-              <p className="font-roboto text-[24px] text-black font-normal mb-2">
-                Nothing to see here... yet! To see this content, you need to {" "}
-                <Link to="/Login" className="text-blue-700 font-medium underline">
-                  log in.
-                </Link>
-              </p>
-            </div>
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <>
+         <Header/>
+           <div  className="flex items-center justify-center">
+             <div className="
+               flex flex-col md:flex-row 
+               items-center 
+               justify-center 
+               gap-4 md:gap-8
+               w-[703px] h-[439px] pb-20 "
+              >
+               <div>
+                   <img src="\src\assets\403.png" alt="Post-it" 
+                   className="
+                     w-[744pxpx] h-[189px] 
+                     md:w-[334.59px] md:h-[439px] 
+                      object-contain " 
+                   />
+               </div >
+         
+                 <div  className="text-center w-[387.41px] h-[72px] ">
+                 <div>
+                  <p className="text-center md:text-start text-7xl md:text-8xl font-normal ">403</p>
+                  <p className="lg:text-start text-lg text-preto font-normal mb-2 ">
+                    Nothing to see here... yet! To see <br /> this content, you need to {} 
+                    <span className="block md:inline mt-2 md:mt-0">
+                      <Link
+                        to="/Login"
+                        className="text-blue-700 underline font-bold md:text-end "
+                      >
+                        log in.
+                      </Link>
+                    </span>
+                  </p>
+                </div>
+              </div>
+             </div>
+           </div>
+         <Footer/>
+       </>
   );
 }
--- project-manager/src/pages/NotFound.tsx
+++ project-manager/src/pages/NotFound.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
             items-center 
             justify-center 
             gap-4 md:gap-8
-            w-[703px] h-[439px]"
+            w-[703px] h-[439px] "
             >
             <div>
                 <img src="\src\assets\404.png" alt="Post-it" 
@@ -26,7 +26,7 @@ export default function NotFound() {
               <div  className="text-center w-[230px] h-[196px]">
                 <p className="text-center md:text-start text-7xl md:text-8xl font-normal">404</p>
                 <p className="text-lg	text-preto mb-2">Oops! Nothing to see here, just a lost kanban task.</p>
-                <div className="md:text-end">
+                <div className="md:text-end ">
                   <Link to="/" className=" text-blue-700 underline font-bold md:text-end">
                   Go back to home 
                   </Link>
