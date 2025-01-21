93ac357113755d962a8cf0dc986fb4a7668cf3e3
nanerseasac <renan.casaeses@gmail.com>
2025-01-21 13:57:39 -0300

    the feature padding responsiviness

--- project-manager/src/components/TheManager.tsx
+++ project-manager/src/components/TheManager.tsx
@@ -4,7 +4,7 @@ const TheManager = () => {
   return (
     <div className="bg-azul-escuro text-white lg:items-center py-[60px] flex flex-col gap-[49px] xl:flex xl:flex-col overflow-hidden">
       <div
-        className="h-[252px] px-[20.5px] lg:px-0 flex flex-col gap-6 md:w-[656px] md:h-[216px] md:mx-auto xl:w-[1100px] xl:h-[192px] xl:flex xl:flex-col xl:gap-6 xl:mx-[170px]
+        className="h-[252px] px-[20.5px] sm:px-[44px] lg:px-0 flex flex-col gap-6 md:w-[656px] md:h-[216px] md:mx-auto xl:w-[1100px] xl:h-[192px] xl:flex xl:flex-col xl:gap-6 xl:mx-[170px]
       "
       >
         <h1 className="text-2xl leading-[48px] text-amarelo-transparente md:text-[40px] ">
@@ -16,7 +16,7 @@ const TheManager = () => {
           and powerful features, you can plan, prioritize, and keep tabs on your
           projects - whether you’re working solo or a with a team
         </p>
-        <button className="mx-auto text-[15px] bg-white text-roxo w-[205px] rounded-lg h-9 f md:mx-0 md:w-[240px] md:text-base lg:w-[240px] lg:h-12 lg:font-semibold hover:bg-azul-dessaturado hover:text-white transition duration-200 ease-in-out">
+        <button className="mx-auto text-[15px] bg-white text-roxo w-[205px] rounded-lg h-9 sm:mx-0 md:w-[240px] md:text-base lg:w-[240px] lg:h-12 lg:font-semibold hover:bg-azul-dessaturado hover:text-white transition duration-200 ease-in-out">
           Learn More
         </button>
       </div>
